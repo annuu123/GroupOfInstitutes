9ed808bdac5744c925de960445e0ab0a00e883ac
annuu123 <166362651+annuu123@users.noreply.github.com>
2025-03-05 11:39:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,49 +1,60 @@
 const express = require('express')
 const path = require('path')
-const app = express()
-const PORT = 8080
+const morgan = require('morgan')              // 3rd party
+const helmet = require('helmet')              // 3rd party
+const cors = require('cors')                  // 3rd party
+const compression = require('compression')    // 3rd party
+const cookieParser = require('cookie-parser') // 3rd party
 
-// Middlewares
 const logger = require('./Middlewares/logger')
 const errorHandler = require('./Middlewares/errorHandler')
 const maintenance = require('./Middlewares/maintenance')
 const requestTimer = require('./Middlewares/requestTimer')
 
-// Core middleware
+const app = express()
+const PORT = 8080
+
+// Core Middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// Custom middlewares
+// 3rd Party Middlewares
+app.use(morgan('dev')) // request logger
+app.use(helmet()) // security headers
+app.use(cors()) // cross-origin
+app.use(compression()) // compress responses
+app.use(cookieParser()) // parse cookies
+
+// Custom Middlewares (your own)
 app.use(logger)
 app.use(requestTimer)
 app.use(maintenance)
 
-// Serve static files from public/
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public')))
 
-// Mount API routes (from nodejsmodules/api)
+// API Routes
 const apiRoutes = require('./api/apiRoutes')
 app.use('/api', apiRoutes)
 
-// Serve login page at root
+// Root route - Login Page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'login.html'))
 })
 
-// Serve dashboard
+// Dashboard
 app.get('/api/dashboard', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'dashboard.html'))
 })
 
-// Serve register page
+// Register page
 app.get('/api/register', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'register.html'))
 })
 
-// Global error handler
+// Global Error Handler
 app.use(errorHandler)
 
-// Start server
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
 })
